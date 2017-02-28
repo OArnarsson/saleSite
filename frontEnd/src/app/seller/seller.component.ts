101eb08d4987e1de7c52d81d4548909e7d36a8fa
OArnarsson <orriarnars@gmail.com>
2017-02-28 21:05:56 +0000

    Layout + basic modal layouts

--- frontEnd/src/app/seller/seller.component.ts
+++ frontEnd/src/app/seller/seller.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-seller',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
 
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) {}
+
+  open(edit) {
+    this.modalService.open(edit).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    });
+  }
 }
