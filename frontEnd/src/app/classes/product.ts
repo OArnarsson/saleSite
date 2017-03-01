736532b91248d1243b91750993310ebb725c755d
OArnarsson <orriarnars@gmail.com>
2017-03-01 16:15:40 +0000

    modal and product update

--- frontEnd/src/app/classes/product.ts
+++ frontEnd/src/app/classes/product.ts
@@ -2,21 +2,23 @@
 export class Product {
     id: number;
     product: {
-      id: number;
-      name: string;
-      price: number;
-      quantitySold: number;
-      quantityInStock: number;
-      imagePath: string;
+        id: number;
+        name: string;
+        price: number;
+        quantitySold: number;
+        quantityInStock: number;
+        imagePath: string;
     }
 
     constructor(id, pId, name, price, quantitySold, quantityInStock, imagePath) {
         this.id = id;
-        this.product.id = pId;
-        this.product.name = name;
-        this.product.price = price;
-        this.product.quantitySold = quantitySold;
-        this.product.quantityInStock = quantityInStock;
-        this.product.imagePath = imagePath;
+        this.product = {
+            'id': pId,
+            'name': name,
+            'price': price,
+            'quantitySold': quantitySold,
+            'quantityInStock': quantityInStock,
+            'imagePath': imagePath
+        };
     }
 }
--- frontEnd/src/app/seller-details/seller-details.component.ts
+++ frontEnd/src/app/seller-details/seller-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Product} from '../classes/product';
+import { Product } from '../classes/product';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DialogComponent } from '../dialog/dialog.component';
 import { ProductService } from '../apiServices/product.service';
 import { SellerService } from '../apiServices/seller.service';
 import { Seller } from '../classes/seller';
@@ -17,11 +19,12 @@ export class SellerDetailsComponent implements OnInit {
   public seller: Seller;
   public products: Product[];
 
-  constructor(private productService: ProductService, private sellerService: SellerService, private router: ActivatedRoute) { }
+
+  constructor(private modalService: NgbModal, private productService: ProductService, private sellerService: SellerService, private router: ActivatedRoute) { }
 
   ngOnInit() {
     this.products = [];
-    this.seller = new Seller(0,'','','');
+    this.seller = new Seller(0, '', '', '');
     this.router.params.subscribe((params) => {
       this.sellerId = +params['id'];
       console.log('got the param: ' + this.sellerId);
@@ -31,18 +34,35 @@ export class SellerDetailsComponent implements OnInit {
 
   getInitData() {
     this.sellerService.getSingleSeller(this.sellerId).subscribe(
-        (seller) => {
-          this.seller = seller;
-          console.log(JSON.stringify(this.seller));
-        }
+      (seller) => {
+        this.seller = seller;
+        console.log(JSON.stringify(this.seller));
+      }
     );
 
     this.productService.getProductsById(this.sellerId).subscribe(
-        (products) => {
-          this.products = products;
-          console.log(JSON.stringify(this.products));
-        }
+      (products) => {
+        this.products = products;
+        console.log(JSON.stringify(this.products));
+      }
     )
   }
 
+  newProduct() {
+    this.popModal('Create a new product', 'Create', new Product(this.sellerId, 9, '', 0, 0, 0, ''), 'POST');
+  }
+
+  popModal(title: string, okBut: string, product: Product, rest: string) {
+    const newProduct = product;
+    const modal = this.modalService.open(DialogComponent);
+    modal.componentInstance.title = title;
+    modal.componentInstance.onOkButton = okBut;
+    modal.componentInstance.product = newProduct;
+    modal.result.then(obj => {
+      //this.addProductToDb(obj, rest);
+      console.log('modal closed successfully');
+    }).catch(err => {
+      console.log(err);
+    })
+  }
 }
